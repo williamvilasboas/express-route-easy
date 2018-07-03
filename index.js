92985b97bcc701498705d0cdeb6088d1ae1e4048
William Vilas Boas <william.vboas@gmail.com>
2018-07-03 19:58:58 -0300

    New version

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+module.exports = (accepts, exportsObject, expressRouter = express.Router()) => {
+
+  const router = global.express ? global.express.Router() : expressRouter
+  const routerGen = (url, method, callback) => router[method](url, callback)
+
+  for (const accept of accepts) {
+    if (typeof accept === 'string') {
+      router.get(accept, exportsObject[accept.name])
+    } else {
+      routerGen(accept.url, accept.method, exportsObject[accept.name])
+    }
+  }
+
+  const dynamicsRoutes = Object.keys(exportsObject).filter((key) => Array.isArray(exportsObject[key]))
+  if (dynamicsRoutes.length) {
+    for (const dynamicKey of dynamicsRoutes) {
+      const dynamicRoute = exportsObject[dynamicKey]
+      const dynamicItemsLength = dynamicRoute.length
+      const url = dynamicRoute[0]
+      const options = typeof dynamicRoute[1] === 'object' && !Array.isArray(dynamicRoute[1]) ? dynamicRoute[1] : {method: 'get'}
+      const callback = dynamicRoute[dynamicItemsLength - 1]
+
+      routerGen(url, options.method, callback)
+    }
+  }
+
+  return router
+}
